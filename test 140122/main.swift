f2613ed7df246c5e3663d9a7a3002b67b2a78055
Memka <mkasenova87@gmail.com>
2023-01-16 11:39:47 +0600

    изм

--- test 140122/main.swift
+++ test 140122/main.swift
@@ -7,5 +7,46 @@
 
 import Foundation
 
-print("Hello, World!")
+//1) С помощью циклов сделайте так, чтобы выходило сообщение введите имя: и создавал нам ридлайн 5 раз. Каждый раз когда мы нажимаем enter, программа должна здороваться с нами по имени. Например: "Привет, Бегимай"
+
+var name: String = "Kanykei"
+
+
+for i in 1...5 {
+    print("Введите имя:")
+    var searchName = readLine()!
+    print("Привет\(name)")
+}
+
+    
+    //2)Создайте пустой массив типа String. В списке 14 студентов. Сделайте программу, так чтобы нам 14 раз просило ввести имя: Введите имя каждого студента по очереди и имя должно добавляться в массив. В конце запринтуйте ваш массив с именами. Там должны быть все имена.
+
+
+var students:[String] = []
+for i in 1...14 {
+    print("Введите имя:")
+}
+students.append ("Aigerim")
+students.append ("Samara")
+students.append ("Fedya")
+
+students.append ("GAGA")
+students.append ("Maka")
+students.append ("Dima")
+
+students.append ("Tima")
+students.append ("Nina")
+students.append ("Yana")
+
+students.append ("Olga")
+students.append ("Kolya")
+students.append ("Rima")
+
+students.append ("Toxa")
+students.append ("Rido")
+
+for item in students {
+    print(item)
+}
+
 
